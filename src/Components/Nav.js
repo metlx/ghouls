7fee5e475fc95c7cdbbe4df54fa44ef289ad2f0c
metlx <towers1337@gmail.com>
2022-07-16 18:44:08 -0700

    'reword'

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -2,26 +2,23 @@
 // import colorLogo from '../Images/color_logo_tinder.png'
 import sidsSkull from '../Images/sids_skull.jpeg'
 
-const Nav = ({ minimal, authToken, setShowModal, showModal}) => {
+const Nav = ({ minimal, authToken, setShowModal, showModal }) => {
 
-    const handleClick = () => {
-        setShowModal(true)
-    }
+  const handleClick = () => {
+    setShowModal(true)
+  }
 
-    return (
-        <nav className="logo-container">
-          <img alt="img is broken cuss" className="logo" src={ minimal ? sidsSkull : sidsSkull } />
-
-          {!authToken && !minimal &&
-           <button
-             className='nav-button'
-             onClick={handleClick}
-             disabled={showModal}
-           >Log In</button>}
-
-        </nav>
-
-    )
+  return (
+    <nav className="logo-container">
+      <img alt="img is broken cuss" className="logo" src={minimal ? sidsSkull : sidsSkull} />
+      {!authToken && !minimal &&
+        <button
+          className='nav-button'
+          onClick={handleClick}
+          disabled={showModal}
+        >Log In</button>}
+    </nav>
+  )
 }
 
 export default Nav
